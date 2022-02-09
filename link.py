abfdde928ff98dd41afbad76bc6628d65bcc88ef
Wenyang Duan <wenyang.duan@gmail.com>
2022-02-09 15:40:50 -0800

    problem: link reverse [done]

--- link.py
+++ link.py
@@ -9,6 +9,12 @@ class Link:
     >>> link
     (2, (1, None))
     '''
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+
+    def __repr__(self):
+        return f'({self.head}, {self.tail})'
 
 def get_link(lst):
     '''
@@ -16,9 +22,21 @@ def get_link(lst):
     >>> get_link((1, 2, 3))
     (1, (2, (3, None)))
     '''
+    if not lst:
+        return None
+    link = None
+    for i in lst[::-1]:
+        link = Link(i, link)
+    return link
 
 def reverse_link(link):
     '''
     >>> reverse_link(get_link((1, 2, 3)))
     (3, (2, (1, None)))
     '''
+    prv, cur, nxt = None, link, link.tail if link else None
+    while cur:
+        pprv = prv
+        prv, cur, nxt = cur, nxt, nxt.tail if nxt else None
+        prv.tail = pprv
+    return prv
