d38f41875d59a27386f54a7c3de15f7557c4d902
Wenyang Duan <wenyang.duan@gmail.com>
2022-02-10 21:40:18 -0800

    problem: stack

---
+++ stack.py
@@ -0,0 +1,13 @@
+class Stack:
+    '''
+    >>> stack = Stack()
+    >>> stack.push(1)
+    >>> stack.pop()
+    1
+    >>> stack.push(1)
+    >>> stack.push(2)
+    >>> stack.pop()
+    2
+    >>> stack.pop()
+    1
+    '''
