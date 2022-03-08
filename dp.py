1aca59f105f80291688b6fc11df6ee9b55208cd0
eecsmap <71915887+eecsmap@users.noreply.github.com>
2022-03-07 20:24:48 -0800

    Update dp.py

--- dp.py
+++ dp.py
@@ -339,3 +339,36 @@ def egg_recur(d, k):
         return d
     return egg(d, k - 1) + egg(d - 1, k)
 
+
+def balloon_tab(numbers):
+    '''
+    >>> balloon_tab([1,2,3])
+    12
+    >>> balloon_tab([4,5,7])
+    175
+    >>> balloon_tab([2,3,100,4])
+    1412
+    '''
+    return 
+
+def balloon(numbers):
+    '''
+    >>> balloon([1,2,3])
+    12
+    >>> balloon([4,5,7])
+    175
+    >>> balloon([2,3,100,4])
+    1412
+    '''
+    if len(numbers) == 1:
+        return numbers[0]
+    if len(numbers) == 2:
+        return numbers[0] * numbers[1] + max(numbers)
+    def value(numbers, i):
+        v = numbers[i]
+        if i > 0:
+            v *= numbers[i - 1]
+        if i < len(numbers) - 1:
+            v *= numbers[i + 1]
+        return v
+    return max(balloon(numbers[:i] + numbers[i+1:]) + value(numbers, i) for i in range(len(numbers)))
